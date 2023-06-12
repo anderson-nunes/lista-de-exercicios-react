27842da43da76321bdd3d036e02e5c3b875cdd80
anderson-nunes <andersonribeironunes@hotmail.com>
2023-06-12 13:51:08 -0300

    Lista de exercicios usando useState, useEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import ExercicioUseState from './Exerc/ExercicioUseState';
-import Exerc from './Exerc/HomeExercicios'
-import Exercicios from "./Exercicios/Exercicios";
+import Lista01 from './Lista01/HomeExercicios'
+import Lista02 from './Lista02/HomeExercicioUseState'
+import Lista03 from './Lista03/Exercicios'
+
 
 function App() {
   return (
     <div>
-      <Exercicios />
-      <Exerc />
-      <ExercicioUseState />
+      <Lista01 />
+      <Lista02 />
+      <Lista03 />
     </div>
   );
 }
